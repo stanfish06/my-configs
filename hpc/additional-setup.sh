17f988ebcacc467bdf219d7f454adc663b5842aa
Stan <zyyu@umich.edu>
2025-06-14 21:35:15 -0400

    small fix, globus cli not working

--- hpc/additional-setup.sh
+++ hpc/additional-setup.sh
@@ -2,14 +2,9 @@
 
 command_list=("compute" "transfer")
 if [[ "$1" == ${command_list[0]} ]]; then
-    module purge
     module load use.own fzf uv matlab python/3.12
 elif [[ "$1" == ${command_list[1]} ]]; then
-    module purge
-    module load use.own python fzf globus-cli rclone
-    if type globus > /dev/null 2>&1; then
-        eval "$(globus --bash-completer)"
-    fi
+    module load use.own python fzf rclone
 else
     echo "Invalid command. Available options are: ${command_list[*]}"
 fi
