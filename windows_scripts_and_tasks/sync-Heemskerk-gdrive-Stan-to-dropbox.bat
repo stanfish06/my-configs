99d1a03a0b287690f4cec6d00857cc1a2e3ac254
stanfish06 <93099613+stanfish06@users.noreply.github.com>
2025-06-21 18:31:36 -0400

    update sync scripts

--- windows_scripts_and_tasks/sync-Heemskerk-gdrive-Stan-to-dropbox.bat
+++ windows_scripts_and_tasks/sync-Heemskerk-gdrive-Stan-to-dropbox.bat
@@ -1,1 +1,2 @@
 rclone copy stanfish06:HeemskerkLabFiles/Stan stan-umich:Heemskerk-gdrive-Stan-backup
+rclone copy stanfish06:HeemskerkLabFiles/Stan dropbox-personal:Heemskerk-gdrive-Stan-backup
---
+++ windows_scripts_and_tasks/sync-Heemskerk-gdrive-to-S3.bat
@@ -0,0 +1,1 @@
+rclone copy stanfish06:HeemskerkLabFiles s3-aws:stan-data-misc/HeemskerkLabFiles
