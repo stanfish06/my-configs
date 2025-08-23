ab4148e9fd6afb07e256604b1089d7251b8356b8
stanfish06 <93099613+stanfish06@users.noreply.github.com>
2025-08-23 01:55:53 -0400

    update hotspot

--- powershell/hotspot.ps1
+++ powershell/hotspot.ps1
@@ -1,5 +1,5 @@
 param(
-    [ValidateSet("Start","Stop","EnableAlwaysOn","DisableAlwaysOn","Status")]
+    [ValidateSet("Start","Stop","Status")]
     [string]$Action
 )
 
@@ -50,51 +50,6 @@ function Stop-Hotspot {
     }
 }
 
-function Enable-AlwaysOn {
-    $taskName = "HotspotAlwaysOn"
-    $scriptPath = $MyInvocation.MyCommand.Path
-    $startBoundary = (Get-Date).ToString("yyyy-MM-dd'T'HH:mm:ss")
-    $xml = @"
-<?xml version="1.0" encoding="UTF-16"?>
-<Task version="1.4" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
-  <Triggers>
-    <TimeTrigger>
-      <Repetition>
-        <Interval>PT5M</Interval>
-        <StopAtDurationEnd>false</StopAtDurationEnd>
-      </Repetition>
-      <StartBoundary>$startBoundary</StartBoundary>
-      <Enabled>true</Enabled>
-    </TimeTrigger>
-  </Triggers>
-  <Principals>
-    <Principal id="Author">
-      <RunLevel>HighestAvailable</RunLevel>
-    </Principal>
-  </Principals>
-  <Actions Context="Author">
-    <Exec>
-      <Command>powershell.exe</Command>
-      <Arguments>-ExecutionPolicy Bypass -File `"$scriptPath`" -Action Start</Arguments>
-    </Exec>
-  </Actions>
-</Task>
-"@
-
-    $tempXml = "$env:TEMP\HotspotTask.xml"
-    Set-Content -Path $tempXml -Value $xml -Encoding Unicode
-    schtasks /Create /TN $taskName /XML $tempXml /F | Out-Null
-    Remove-Item $tempXml
-    Write-Host "Always-On enabled (checks every 5 min)" -ForegroundColor Green
-}
-
-
-function Disable-AlwaysOn {
-    $taskName = "HotspotAlwaysOn"
-    schtasks /Delete /TN $taskName /F | Out-Null
-    Write-Host "Always-On disabled" -ForegroundColor Yellow
-}
-
 function Show-Status {
     $taskExists = schtasks /Query /TN "HotspotAlwaysOn" 2>$null
     if ($LASTEXITCODE -eq 0) {
@@ -111,8 +66,6 @@ function Show-Status {
 switch ($Action) {
     "Start" { Start-Hotspot }
     "Stop" { Stop-Hotspot }
-    "EnableAlwaysOn" { Enable-AlwaysOn }
-    "DisableAlwaysOn" { Disable-AlwaysOn }
     "Status" { Show-Status }
 }
 # run powershell.exe -ExecutionPolicy Bypass -File "./hotspot.ps1" -Action something
---
+++ powershell/start_hotspot.bat
@@ -0,0 +1,2 @@
+REM create task as SYSTEM, then the window wont pop up
+powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:/Users/zhiyu/Desktop/Git/my-configs/powershell/hotspot.ps1" -Action Start
