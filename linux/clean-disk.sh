bc76ab4f7ded548ddcd907c84cd72b2c5d8b7b0b
stanfish <stanfish@stan.stanfish.com>
2025-10-13 02:44:39 -0400

    update

--- linux/clean-disk.sh
+++ linux/clean-disk.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # some trash locations
-rm -rf /var/cache/
+# rm -rf /var/cache/
 rm -rf ~/.cache/
 journalctl --vacuum-time=7d
 
 # pacman
-pacman -Scc --noconfirm
+if command -v "pacman" >/dev/null 2>&1; then
+  pacman -Scc --noconfirm
+elif command -v "apt" >/dev/null 2>&1; then
+  apt autoclean
+  apt autoremove
+  apt update
+fi
+
