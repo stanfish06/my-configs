dd28ace6f076f13ec1688ec1ec0d541ce5518dfb
Stan <zhiyuanyu06@gmail.com>
2026-05-06 06:47:39 -0400

    gentoo

---
+++ gentoo/configs-TF-X220/backup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# emerge
+emerge --info > emerge-info.txt
+# var
+cp /var/lib/portage/world ./var-lib-portage/
+# etc
+cp -r /etc/X11/xorg.conf.d ./etc-TF-X220/X11/
+cp /etc/portage/make.conf ./etc-TF-X220/portage/
+cp -r /etc/portage/package.use ./etc-TF-X220/portage/
+cp -r /etc/portage/package.license ./etc-TF-X220/portage/
+cp /etc/fstab ./etc-TF-X220/
