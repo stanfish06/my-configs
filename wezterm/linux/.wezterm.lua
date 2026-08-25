9630fd985bae367daca3bcead965a917cadcc88d
Stan <zyyu@umich.edu>
2026-08-25 03:02:06 -0400

    update

--- wezterm/linux/.wezterm.lua
+++ wezterm/linux/.wezterm.lua
@@ -43,12 +43,12 @@ config.custom_block_glyphs = false -- this allows you do display ░▒▓ prope
 -- config.font = wezterm.font("Maple Mono NF", { weight = "Regular", italic = false })
 -- config.line_height = 0.875
 -- Victor Mono (looking nice for regular chars but too twisted for italic and thin, better to use maple for italic)
-config.font = wezterm.font("VictorMono Nerd Font Mono", { weight = "DemiBold", italic = false })
-config.line_height = 0.885
--- config.font = wezterm.font("Iosevka", { stretch = "SemiExpanded", weight = "Regular" })
+-- config.font = wezterm.font("VictorMono Nerd Font Mono", { weight = "DemiBold", italic = false })
+-- config.line_height = 0.885
+config.font = wezterm.font("Iosevka", { stretch = "SemiExpanded", weight = "Regular" })
 -- config.font = wezterm.font("IosevkaTerm Nerd Font", { stretch = "SemiExpanded", weight = "Regular" })
-config.font = wezterm.font("GeistMono Nerd Font", { weight = "Regular" })
-config.line_height = 1.05
+-- config.font = wezterm.font("GeistMono Nerd Font", { weight = "Regular" })
+-- config.line_height = 1.05
 -- config.font = wezterm.font("Perfect Dos Vga 437 Win", { weight = "DemiLight", italic = false })
 -- more complex settings
 -- only appenlied to italic words
